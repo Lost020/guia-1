f1dfd36521608926868d784bae6da813b72951d0
Nelson Fernando Munoz Hernandez <nelson.munoz21@inacapmail.cl>
2024-09-02 17:52:52 -0400

    comentarios

--- app1/views.py
+++ app1/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hola, mundo. Esta es la página de inicio de nombreapp.")
+    return HttpResponse("<h1>Hola, mundo. Esta es la página de inicio de nombreapp.</h1> <br> <p>el titulo tuve que corregirlo yo agregando los <h1> inicial y </h1> final, <br>         atte nelsito</p>")
 
 # Create your views here.
